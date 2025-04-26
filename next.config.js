da7710448bd2d5edb98f3033bbd841b7985eb944
Trendi <168600517+Trendiisales@users.noreply.github.com>
2025-04-27 07:18:56 +1200

    fix: reset config, switch to static export, fix all imports

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true
-  },
   webpack: (config) => {
     config.resolve.alias['@'] = require('path').resolve(__dirname, 'src');
     return config;
-  },
-  output: 'export'
+  }
 };
 
 module.exports = nextConfig;
--- src/components/composer/CaptionSelection.tsx
+++ src/components/composer/CaptionSelection.tsx
@@ -1,4 +1,4 @@
-﻿import React, { useState } from 'react';
+import React, { useState } from 'react';
 import { CaptionSelectionProps } from '@/types';
 
 const captions = [
